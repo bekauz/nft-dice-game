e0350ee0f6d5f040bb7dbac2638b8ff98be7cbb7
bekauz <bekauz@protonmail.com>
2022-02-05 15:14:50 +0100

    connecting wallet on frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ethers } from "ethers";
+import { Web3Provider } from '@ethersproject/providers';
+
+declare var window: any
 
 function App() {
+
+  const [currentAccount, setCurrentAccount] = useState<string | undefined>(undefined);
+
+  const checkWalletConnection =  async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("no wallet found");
+        return;
+      } else {
+        console.log("ethereum object: ", ethereum);
+        // Web3 provider injected as window.ethereum into each page by Metamask 
+        const provider: Web3Provider = new ethers.providers.Web3Provider(window.ethereum)
+        // request permission to connect user accounts
+        const accounts: any[] = await provider.send("eth_requestAccounts", []);
+        
+        if (accounts.length > 0) {
+          const account: string = accounts[0];
+          console.log('Found an authorized account:', account, typeof account);
+          setCurrentAccount(account);
+        } else {
+          console.log("No account found.");
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    checkWalletConnection();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
