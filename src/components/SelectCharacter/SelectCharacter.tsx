ad22202a81448da4d74ab88e3ee4d014438705e3
bekauz <bekauz@protonmail.com>
2022-02-08 18:19:42 +0100

    character selection screen; adding char mint events to effect hooks

--- src/components/SelectCharacter/SelectCharacter.tsx
+++ src/components/SelectCharacter/SelectCharacter.tsx
@@ -9,8 +9,23 @@ declare var window: any
 
 const SelectCharacter = ({ setCharacterNFT }) => {
 
-  const [availableCharacters, setAvailableCharacters] = useState(null);
+  const [availableCharacters, setAvailableCharacters] = useState<any[]>([]);
   const [gameContract, setGameContract] = useState<Contract | undefined>(undefined);
+
+  const mintCharacterNFT = (charId) => async () => {
+    if (gameContract) {
+      console.log("Minting character:");
+      try {
+        const mintTxn = await gameContract.mintCharacterNFT(charId);
+        await mintTxn.wait();
+        console.log("transaction minted: ", mintTxn);
+      } catch(err) {
+        console.warn("Mint error:", err);
+      }
+    } else {
+      console.log("No contract available");
+    }
+  };
 
   useEffect(() => {
     const { ethereum } = window;
@@ -46,15 +61,55 @@ const SelectCharacter = ({ setCharacterNFT }) => {
       setAvailableCharacters(availableCharacters);
     };
 
+    const onCharacterMintEvent = async (sender: any, tokenId: string, characterId: string) => {
+      console.log(`Character NFT successfully minted by: ${sender} \n
+        with tokenId: ${tokenId} \nand characterId: ${characterId}`);
+      
+      if (gameContract) {
+        const userCharacterNFT = await gameContract.getUserNFT();
+        console.log("User owns character nft: ", userCharacterNFT);
+        setCharacterNFT(transformCharacterData(userCharacterNFT));
+      }
+    };
     // if hook triggered and gameContract is nonnull, get characters
     if (gameContract) {
       getCharacters();
+      gameContract.on('CharacterMint', onCharacterMintEvent);
     }
+
+    return () => {
+      // disable listener upon component unmount
+      if (gameContract) {
+        gameContract.off('CharacterMint', onCharacterMintEvent);
+      }
+    };
   }, [gameContract]);
+
+  const renderCharacterSelection = () => (
+    availableCharacters.map(
+      (character, index) => (
+        <div className="character-selection-box">
+          <img src={character.imageURI} alt={character.name}></img>
+          <h4>{character.name}</h4>
+          <ul className="character-attributes">
+            <li>Wager size: {character.wagerSize}</li>
+            <li>Starting funds: {character.maxFunds}</li>
+          </ul>
+          <button
+            type="button"
+            className="character-mint-button"
+            onClick={mintCharacterNFT(index)}
+          >Mint {character.name}</button>
+        </div>
+    ))
+  );
 
   return (
     <div className="select-character-container">
       <h2>Select your character:</h2>
+      <div className="character-mint-options">
+        {renderCharacterSelection()}
+      </div>
     </div>
   );
 };
